867418ccfced6902c4c9056770ca180ee03dfdaa
journeyH <707184737@qq.com>
2017-02-16 14:51:28 +0800

    first

---
+++ js/main.js
@@ -0,0 +1,196 @@
+﻿window.onload=function(){
+function $(id){return document.getElementById(id)}
+var menu=$("topTags").getElementsByTagName("ul")[0];//顶部菜单容器
+var tags=menu.getElementsByTagName("li");//顶部菜单
+var ck=$("leftMenu").getElementsByTagName("ul")[0].getElementsByTagName("li");//左侧菜单
+var j;
+//点击左侧菜单增加新标签
+for(i=0;i<ck.length;i++){
+ck[i].onclick=function(){
+$("welcome").style.display="none"//欢迎内容隐藏
+clearMenu();
+this.style.background='url(images/tabbg02.gif)'
+//循环取得当前索引
+for(j=0;j<8;j++){
+if(this==ck[j]){
+if($("p"+j)==null){
+openNew(j,this.innerHTML);//设置标签显示文字
+ }
+clearStyle();
+$("p"+j).style.background='url(images/tabbg1.gif)';
+clearContent();
+$("c"+j).style.display="block";
+   }
+ }
+return false;
+  }
+ }
+//增加或删除标签
+function openNew(id,name){
+var tagMenu=document.createElement("li");
+tagMenu.id="p"+id;
+tagMenu.innerHTML=name+"&nbsp;&nbsp;"+"<img src='images/off.gif' style='vertical-align:middle'/>";
+//标签点击事件
+tagMenu.onclick=function(evt){
+clearMenu();
+ck[id].style.background='url(images/tabbg02.gif)'
+clearStyle();
+tagMenu.style.background='url(images/tabbg1.gif)';
+clearContent();
+$("c"+id).style.display="block";
+}
+//标签内关闭图片点击事件
+tagMenu.getElementsByTagName("img")[0].onclick=function(evt){
+evt=(evt)?evt:((window.event)?window.event:null);
+if(evt.stopPropagation){evt.stopPropagation()} //取消opera和Safari冒泡行为;
+this.parentNode.parentNode.removeChild(tagMenu);//删除当前标签
+var color=tagMenu.style.backgroundColor;
+//设置如果关闭一个标签时，让最后一个标签得到焦点
+if(color=="#ffff00"||color=="yellow"){//区别浏览器对颜色解释
+if(tags.length-1>=0){
+clearStyle();
+tags[tags.length-1].style.background='url(images/tabbg1.gif)';
+clearContent();
+var cc=tags[tags.length-1].id.split("p");
+$("c"+cc[1]).style.display="block";
+clearMenu();
+ck[cc[1]].style.background='url(images/tabbg1.gif)';
+ }
+else{
+clearContent();
+clearMenu();
+$("welcome").style.display="block"
+   }
+  }
+}
+menu.appendChild(tagMenu);
+}
+//清除菜单样式
+function clearMenu(){
+for(i=0;i<ck.length;i++){
+ck[i].style.background='url(images/tabbg01.gif)';
+ }
+}
+//清除标签样式
+function clearStyle(){
+for(i=0;i<tags.length;i++){
+menu.getElementsByTagName("li")[i].style.background='url(images/tabbg2.gif)';
+  }
+}
+//清除内容
+function clearContent(){
+for(i=0;i<7;i++){
+$("c"+i).style.display="none";
+ }
+}
+}
+
+
+
+
+var  highlightcolor='#d5f4fe';
+//此处clickcolor只能用win系统颜色代码才能成功,如果用#xxxxxx的代码就不行,还没搞清楚为什么:(
+var  clickcolor='#51b2f6';
+function  changeto(){
+source=event.srcElement;
+if  (source.tagName=="TR"||source.tagName=="TABLE")
+return;
+while(source.tagName!="TD")
+source=source.parentElement;
+source=source.parentElement;
+cs  =  source.children;
+//alert(cs.length);
+if  (cs[1].style.backgroundColor!=highlightcolor&&source.id!="nc"&&cs[1].style.backgroundColor!=clickcolor)
+for(i=0;i<cs.length;i++){
+	cs[i].style.backgroundColor=highlightcolor;
+}
+}
+
+function  changeback(){
+if  (event.fromElement.contains(event.toElement)||source.contains(event.toElement)||source.id=="nc")
+return
+if  (event.toElement!=source&&cs[1].style.backgroundColor!=clickcolor)
+//source.style.backgroundColor=originalcolor
+for(i=0;i<cs.length;i++){
+	cs[i].style.backgroundColor="";
+}
+}
+
+function  clickto(){
+source=event.srcElement;
+if  (source.tagName=="TR"||source.tagName=="TABLE")
+return;
+while(source.tagName!="TD")
+source=source.parentElement;
+source=source.parentElement;
+cs  =  source.children;
+//alert(cs.length);
+if  (cs[1].style.backgroundColor!=clickcolor&&source.id!="nc")
+for(i=0;i<cs.length;i++){
+	cs[i].style.backgroundColor=clickcolor;
+}
+else
+for(i=0;i<cs.length;i++){
+	cs[i].style.backgroundColor="";
+}
+}
+
+
+
+
+function getCookie(c_name)
+{
+if (document.cookie.length>0)
+{ 
+c_start=document.cookie.indexOf(c_name + "=")
+if (c_start!=-1)
+{ 
+c_start=c_start + c_name.length+1 
+c_end=document.cookie.indexOf(";",c_start)
+if (c_end==-1) c_end=document.cookie.length
+return unescape(document.cookie.substring(c_start,c_end))
+} 
+}
+return ""
+}
+
+function setCookie(c_name,value,expiredays)
+{
+var exdate=new Date()
+exdate.setDate(exdate.getDate()+expiredays)
+document.cookie=c_name+ "=" +escape(value)+
+((expiredays==null) ? "" : "; expires="+exdate.toGMTString())
+}
+
+function checkCookie()
+{
+username=getCookie('username')
+if (username!=null && username!="")
+  {alert('Welcome again '+username+'!')}
+else 
+  {
+  username=prompt('Please enter your name:',"")
+  if (username!=null && username!="")
+    {
+    setCookie('username',username,365)
+    }
+  }
+}
+
+
+
+var kaiguan=1;
+onclick=function(){
+   if(kaiguan){
+      alert('见证奇迹的时候到了~');
+      kaiguan = 0;
+  }
+}
+
+
+
+$(document).ready(function(){
+  $("button").click(function(){
+  $(this).hide();
+});
+});
